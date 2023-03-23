f952ce399ce27f3eabbaaaf41cb0d4a53b25c13e
atah <habibiatah@gmail.com>
2023-03-23 04:28:22 -0700

    completed the review project

--- REACT-FUNDAMENTAL-PROJECTS/03-reviews/src/Alternative.jsx
+++ REACT-FUNDAMENTAL-PROJECTS/03-reviews/src/Alternative.jsx
@@ -9,16 +9,25 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const {name,job,image,text}=people[index]
 
+  const checkNumber=(number)=>{
+    if(number>people.length-1){
+      return 0;
+    }
 
+    if(number<0){
+      return people.length-1;
+    }
+
+    return number;
+  }
 
   
 
   const nextPerson=()=>{
 
     setIndex((currentIndex)=>{
-      const newIndex=(currentIndex+1)%people.length;
-      console.log(newIndex);
-      return newIndex;
+      const newIndex=currentIndex+1;
+      return checkNumber(newIndex);
      
     })
     
@@ -28,11 +37,11 @@ const App = () => {
   const prevPerson=()=>{
 
     setIndex((currentIndex)=>{
-      const newIndex=(currentIndex- 1 + people.length)%people.length;
-      console.log(newIndex);
-      return newIndex;
+      const newIndex=currentIndex-1;
+      return checkNumber(newIndex);
       
     })
+
 
   }
 
@@ -43,11 +52,7 @@ const App = () => {
       value=index+1;
     }
 
-    const newIndex=value%people.length;
-
-    setIndex(newIndex);
-
-   
+    setIndex(checkNumber(value))
 
   }
   
