c0ac5726285d161e0a06d0e06b320dd66d8327b0
atah <habibiatah@gmail.com>
2023-03-30 07:45:21 -0700

    project in progress

---
+++ COMPLEX-PROJECTS/comfy-sloth/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+import styled from 'styled-components'
+
+function App() {
+  return <div>
+    <h1>comfy store</h1>
+
+  
+
+  </div>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default App
