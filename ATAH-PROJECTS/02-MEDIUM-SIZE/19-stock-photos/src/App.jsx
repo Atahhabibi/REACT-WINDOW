82c045fb1a0b4d0c566c7c2af312b17ddbe70e65
atah <habibiatah@gmail.com>
2023-04-17 07:59:06 -0700

    project in progress

---
+++ ATAH-PROJECTS/02-MEDIUM-SIZE/19-stock-photos/src/App.jsx
@@ -0,0 +1,16 @@
+const mainUrl = `https://api.unsplash.com/photos/`
+const searchUrl = `https://api.unsplash.com/search/photos/`
+
+const ACCESS_KEY=process.env.REACT_APP_API_ACCESS_KEY;
+const SECRET_KEY=process.env.REACT_APP_API_SECRET_KEY;
+
+
+
+function App() {
+
+  return <div>
+    <h1>App Components</h1>
+  </div>
+}
+
+export default App
