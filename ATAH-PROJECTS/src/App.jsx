effccc5a26f15450897ded53095b46c3ea1fa1d2
atahhabibi <habibiatah@gmail.com>
2023-04-18 12:20:45 -0700

    commit in rect folder

--- ATAH-PROJECTS/src/App.jsx
+++ ATAH-PROJECTS/src/App.jsx
@@ -1,25 +1,5 @@
-import useAxios from "./utils/useAxios";
-const mainUrl = `/photos`
-const searchUrl = `/search/photos/`
 
 function App() {
-
-  const {data,isLoading,isError}=useAxios(mainUrl);
-
-
-  if(isLoading){
-    
-  }
-
-
-
-  
-
-
-
-
-
-
   return <div>
     <h1>App Components</h1>
   </div>
